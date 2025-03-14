74bdf0715cfdf4270d4ef3c621f04e2662ba4f3b
Oliver Woolland <oliver.woolland@manchester.ac.uk>
2025-03-14 16:13:00 +0000

    make letter count accept file not string

--- galaxy/tools/examples/lc/letter_count.py
+++ galaxy/tools/examples/lc/letter_count.py
@@ -3,12 +3,15 @@ import sys
 
 # Create the parser and add the arguments
 parser = argparse.ArgumentParser(description='A script that takes a string and writes out the length of each word in the string.')
-parser.add_argument('input_str', help='The string to process')
+parser.add_argument('input_file', help='The file to process')
 parser.add_argument('output_file', help='The name of the file to write the output to')
 args = parser.parse_args()
 
+# Read the input file
+with open(args.input_file, 'r') as f:
+    words = f.read().split()
+    
 # List comprehension to get the length of each word in the string
-words = args.input_str.split()
 word_lengths = [str(len(word)) for word in words]
 
 # Print the word lengths to the console
@@ -17,4 +20,3 @@ print(f"Word lengths: {', '.join(word_lengths)}")
 # Write the word lengths to the output file 
 with open(args.output_file, 'w') as f:
     f.write('\n'.join(word_lengths))
-
